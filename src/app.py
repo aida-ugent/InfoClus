dcfd71e78369fcac19a3afd08c4944bf2f6b3a98
Fuyin Lai <laifuyin69@gmail.com>
2024-10-24 15:22:59 +0200

    update

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "InfoClus | A Dashboard for explainable clustering helping you understand your dataset better"
+app.css.config.serve_locally = False
+my_css_urls = ["https://codepen.io/rmarren1/pen/mLqGRg.css"]
+for url in my_css_urls:
+    app.css.append_css({
+        "external_url": url
+    })
+
