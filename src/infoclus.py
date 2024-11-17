b9d27d4ae0518b55bf9471e499e233f40a9e325a
Fuyin Lai <laifuyin69@gmail.com>
2024-11-17 22:57:33 +0100

    update

--- src/infoclus.py
+++ src/infoclus.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from sklearn.cluster import AgglomerativeClustering
+from adata_utils import generate_adata
+import numpy as np
+import os
+
+
+RUNTIME_OPTIONS = [0.01, 0.5, 1, 5, 10, 30, 60, 180, 300, 600, 1800, 3600, np.inf]
 
 
 class InfoClus:
@@ -8,32 +14,54 @@ class InfoClus:
         It uses a pre-clustering model to offer candidate clusters
 
         Attributes:
-        name (str): The name of the dataset.
-        data (pd.DataFrame): The scaled data of the dataset.
-        emb_name (str): The embedding to be used for clustering.
-        model : The pre-clustering model to be used to offer candidate clusters.
+        name: name of the dataset
+        relative_data_path: the path to the data folder
+        emb_name: the embedding to be used for clustering
+        model: the pre-clustering model to be used to offer candidate clusters
+
+        adata: the adata object that contains the dataset and some related information
 
     '''
 
-    # todo: when dealing with the adata, using get_func defined in the adata_utils.py
-    def __init__(self, name: str, data: pd.DataFrame,
+    def __init__(self, dataset_name: str, relative_data_path: str,
                  main_emb: str = 'tSNE_1',
-                 model = AgglomerativeClustering(linkage='single', distance_threshold=0, n_clusters=None)
+                 model = AgglomerativeClustering(linkage='single', distance_threshold=0, n_clusters=None),
+                 alpha: int = None, beta: float = 1.5, min_att: int = 2, max_att: int = 10, runtime_id: int = 3
                  ):
         '''
 
         :param name: name of the dataset
-        :param data: the scaled data of the dataset
+        :param relative_data_path: the path to the data folder
         :param main_emb: the embedding to be used for clustering
         :param model: the pre-clustering model to be used to offer candidate clusters
         '''
 
-        self.name = name
-        self.data = data
-        # todo: get scaled data, also dataframe
-        # todo: get features types, and ifmixed
+        self.name = dataset_name
+        self.relative_data_path = relative_data_path
         self.emb_name = main_emb
         self.model = model
+
+        self.adata = generate_adata(self.relative_data_path, self.name)
+
+        self.data_raw = pd.DataFrame(self.adata.layers['raw'], columns=self.adata.var_names)
+        self.data_scaled = pd.DataFrame(self.adata.layers['scaled'], columns=self.adata.var_names)
+        self.embedding = self.adata.obsm[self.emb_name]
+        # TODO: check the type of self.var_type
+        self.var_type = self.adata.var['var_type']
+
+        if alpha is None:
+            self.alpha = int(self.adata.obs.size/10)
+        else:
+            self.alpha = alpha
+        self.beta = beta
+        self.min_att = min_att
+        self.max_att = max_att
+        self.runtime_id = runtime_id
+        self.runtime = RUNTIME_OPTIONS[runtime_id]
+        # TODO: check if self_cache_path is correct
+        self.cache_path = os.path.join(self.relative_data_path, self.name, 'cache')
+
+        
 
         # self.emb_name = emb_name
         # self.data = df
